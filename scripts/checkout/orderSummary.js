baf0199779bf6132d4211486ea9927ca5a6ae3ed
arobey <sumiyaakter.arobey@gmail.com>
2025-02-28 22:02:07 +0600

    updating quantity of item in payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -127,13 +127,8 @@ export function renderOrderSummary() {
       const productId = link.dataset.productId;
       removeFromCart(productId);
 
-      const container = document.querySelector(
-        `.js-cart-item-container-${productId}`
-      );
-
-      container.remove();
       updateCartQuantity();
-
+      renderOrderSummary();
       renderPaymentSummary();
     });
   });
